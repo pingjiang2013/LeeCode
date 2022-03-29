03fe050f042c85ec17c2afe9c0d2c0cbcd050b75
pingjiang2013 <pingjiang2013@hotmail.com>
2022-03-29 10:26:12 +0800

    add the LeeCode solution and 0105oneEditAway project

---
+++ 0105oneEditAway/main.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	bool oneEditAway(string first, string second)
+	{
+		if (first.length() > second.length())
+			return (oneEditAway(second, first));
+
+		if (second.length() - first.length() > 1)
+			return false;
+
+		bool bDiff = false;
+
+		if (first.length() == second.length())
+		{
+			for (int i = 0; i < first.length(); ++i)
+			{
+				if (first[i] != second[i])
+				{
+					if (bDiff)
+						return false;
+					else
+						bDiff = true;
+				}
+			}
+			return true;
+		}
+
+		int i = 0;
+		int j = 0;
+		for (; i < first.length(); ++i, ++j)
+		{
+			if (first[i] != second[j])
+			{
+				if (bDiff)
+					return false;
+				else
+					bDiff = true;
+
+				if (first[i] != second[j + 1])
+					return false;
+				else
+					++j;
+			}
+		}
+
+		return true;
+	}
+};
+
+int main()
+{
+	Solution s;
+	bool b = s.oneEditAway("b", "");
+	printf("%s\n", b ? "true" : "false");
+	return 0;
+}
