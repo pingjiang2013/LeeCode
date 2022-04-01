87de00fd13fa4f2cc7e00c22c9fe6037ba0fc398
pingjiang2013 <pingjiang2013@hotmail.com>
2022-04-01 09:34:24 +0800

    split main function and other functions more

--- 0108setZeros/main.cpp
+++ 0108setZeros/main.cpp
@@ -3,35 +3,6 @@
 
 #include "setZeros.h"
 
-void SolutionSetZeros::setZeroes(vector<vector<int>>& matrix) {
-	int m = matrix.size();
-	int n = matrix[0].size();
-
-	vector<bool> row(m), col(n);
-	for (int i = 0; i < m; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			if (0 == matrix[i][j])
-			{
-				row[i] = col[j] = true;
-			}
-		}
-	}
-
-	for (int i = 0; i < m; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			if (row[i] || col[j])
-			{
-				matrix[i][j] = 0;
-			}
-
-		}
-	}
-}
-
 void printMatrixInSetZeros(vector<vector<int>>& matrix)
 {
 	for (int i = 0; i < matrix.size(); ++i)
--- 0204partition/main.cpp
+++ 0204partition/main.cpp
@@ -1,42 +1,6 @@
 #include <stdio.h>
 #include "partition.h"
 
-ListNode* SolutionPartition::partition(ListNode* head, int x) {
-	ListNode* first = new ListNode(0);
-	ListNode* previous = first;
-	first->next = head;
-
-	ListNode* current = head;
-
-	while (NULL != current && current->val < x)
-	{
-		previous = current;
-		current = current->next;
-	}
-	if (NULL == current) return head;
-
-	while (NULL != current)
-	{
-		if (current->val < x)
-		{
-			previous->next = current->next;
-			current->next = first->next;
-			first->next = current;
-			current = previous->next;
-		}
-		else
-		{
-			previous = current;
-			current = current->next;
-		}
-	}
-	ListNode* ret = first->next;
-	delete first;
-
-	return ret;
-}
-
-
 void printListInPartition(ListNode* head)
 {
 	while (NULL != head)
