b63c5166b4dab2041fc59d52a2fd0229df035351
pingjiang2013 <pingjiang2013@hotmail.com>
2022-04-05 10:25:34 +0800

    add printInUnitTest

--- UnitTest/UnitTest01.cpp
+++ UnitTest/UnitTest01.cpp
@@ -6,16 +6,23 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest01
 {
+	const int MAX_LENGTH = 256;
+
 	TEST_CLASS(UnitTest01)
 	{
 	public:
-
-		TEST_METHOD(TestMethod01)
+		void printInUnitTest(const char* fmt, va_list va)
+		{
+			char buf[MAX_LENGTH];
+			sprintf_s(buf, MAX_LENGTH, fmt, va);
+			Logger::WriteMessage(buf);
+		}
+		TEST_METHOD(TestMethod0105)
 		{
 			SolutionOneEditAway s;
+			bool b = s.oneEditAway("b", "");
+			printInUnitTest("%s\n", b ? "true" : "false");
 			Assert::IsTrue(s.oneEditAway("b", ""));
-			Assert::IsFalse(s.oneEditAway("ab", ""));
-			Assert::IsTrue(s.oneEditAway("abc", "abd"));
 		}
 	};
 }
